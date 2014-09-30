9153f573ea92358d17b9ee0b9280d00f10f9f887
Paulo Nunes <amiguencio@icloud.com>
2014-09-30 16:25:09 +0200

    How to added
    Added a how to page to let user know how to send a quote.

--- js/tests.script.js
+++ js/tests.script.js
@@ -4,15 +4,20 @@
 // Here are stored all quotes
 var quotes = [
     "Click. Boom. &iexcl;Asombroso!//Steve Jobs",
-    "El diseño no es sólo cómo se ve o cómo se siente. El Diseño es cómo funciona//Steve Jobs",
+    "El diseño no es sólo cómo se ve o cómo se siente. El Diseño es cómo funciona//Steve Jobs//Paulo Nunes",
     "¿Por qué enrolarse en la armada cuando puedes ser un pirata?//Steve Jobs",
     "La innovación distingue entre un líder y un seguidor//Steve Jobs",
     "La calidad es más importante que la cantidad. Un 'home run' es mucho mejor que dos dobles//Steve Jobs//DaLancelotBruh//twitter",
     "Hay gente que lo único que quiere es ver arder el mundo//Alfred, Batman//DaLancelotBruh//twitter",
     "Si se te da bien algo, nunca lo hagas gratis…//Joker, Batman//DaLancelotBruh//twitter",
     "Ya no puedes fiarte de nadie. Si quieres algo, tienes que hacerlo tú, ¿no crees?//Joker, Batman//DaLancelotBruh//twitter",
-    "No es sino la sombra de una ilusion lo que amas, no puedo darte lo que anhelas//Aragorn//lorenzo.v.matterhorn.948//facebook",
-    "&iexcl;Algebarico!//Finn el Humano, Hora de Aventuras//toofartooblack//tumblr"
+    "No es sino la sombra de una ilusion lo que amas, no puedo darte lo que anhelas//Aragorn, El Se&ntilde;or de los Anillos//lorenzo.v.matterhorn.948//facebook",
+    "&iexcl;Algebraico!//Finn el Humano, Hora de Aventuras//toofartooblack//tumblr",
+    "Veni, vidi, vinci//Julio Cesar//lorenzo.v.matterhorn.948//facebook",
+    "Lo que hacemos en nuestras vidas tiene su eco en la eternidad//Maximo Decimo Meridio, Gladiator//lorenzo.v.matterhorn.948//facebook",
+    "Hazlo, o no lo hagas, pero no lo intentes//Yoda, Star Wars//lorenzo.v.matterhorn.948//facebook",
+    "Relaxing cup of caf&eacute; con leche in Plaza Mayor//Ana Botella//lorenzo.v.matterhorn.948//facebook",
+    "Con el ojo por ojo, al final todos ciegos//Mahatma Gandhi//Alex Coll"
 ];
 // This codeline defines a random number from 1 to the length of the quotes array
 var i = Math.floor(Math.random() * quotes.length),
@@ -56,3 +61,28 @@ if (referal === undefined) {
 }
 document.getElementById("cite").innerHTML = quoteT;
 document.getElementById("author").innerHTML = authorNReferal;
+
+function refresh() {
+    window.location.reload()
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
